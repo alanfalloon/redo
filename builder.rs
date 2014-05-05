3dc9cfe7a50421ba3d04bfe0fbfe641d6c8b7e13
Alan Falloon <alan.falloon@gmail.com>
2014-05-04 23:32:39 -0400

    Complete do-file search iterators
    Extend the iterators to return more than the name of the do-file, and
    clean up the code a bit. Switch to Path from str so that non-utf8 paths
    are properly supported.

--- builder.rs
+++ builder.rs
@@ -1,4 +1,5 @@
 pub fn build(v: &::vars::Vars, targets: Vec<~str>) -> () {
+    let _ = dofile::all_possible_do_files(&Path::new("foo"), &::std::os::getcwd());
     fail!("build {} {}", targets, v.DEPTH)
 }
 
@@ -11,275 +12,259 @@ mod dofile {
     use std::path::Path;
 
     #[deriving(Eq, Clone)]
-    struct DefaultDoFile {
+    struct DoPath {
+        dodir: Path, // The directory containing the do-file
+        basedir: Path, // The dir of the target relative to the do-file
+        dofile: DoFile // The basename, target & extension of the do-file
+    }
+
+    #[deriving(Eq, Clone)]
+    struct DoFile {
         dofile: Path,
         base: Path,
         ext: ~[u8]
     }
 
+    static DOT: u8 = '.' as u8;
+    static DEFAULT: &'static [u8] = bytes!("default");
+    static DO: &'static [u8] = bytes!("do");
+
+    type AllDoPathIter<'a> = ::std::iter::Chain<::std::option::Item<DoPath>, DefaultDoPathIter>;
+    pub fn all_possible_do_files<'a>(filename: &Path, cwd: &Path) -> AllDoPathIter<'a> {
+        let d: Path = Path::new(filename.dirname());
+        let b: Path = Path::new(filename.filename().unwrap());
+        Some(DoPath {
+            dodir: d.clone(),
+            basedir: Path::new(""),
+            dofile: DoFile {
+                dofile: Path::new([b.as_vec(), DO].connect_vec(&DOT)),
+                base: b.clone(),
+                ext: ~[]
+            }
+        }).move_iter().chain(DefaultDoPathIter::new(&d, &b, cwd))
+    }
+
+
+    struct DefaultDoPathIter {
+        dodir: Path,
+        basedir: Path,
+        defaults: DefaultDoFileIter,
+        up: Vec<~[u8]>,
+        basename: Path
+    }
+    impl DefaultDoPathIter {
+        fn new(dodir: &Path, basename: &Path, cwd: &Path) -> DefaultDoPathIter {
+            let abs: Path = cwd.join(dodir);
+            let comps: Vec<~[u8]> = abs.components().map(|x| x.to_owned()).collect();
+            DefaultDoPathIter {
+                dodir: dodir.clone(),
+                basedir: Path::new(""),
+                defaults: DefaultDoFileIter::new(basename),
+                up: comps,
+                basename: basename.clone()
+            }
+        }
+    }
+    impl Iterator<DoPath> for DefaultDoPathIter {
+        fn next(&mut self) -> Option<DoPath> {
+            self.defaults.next()
+                .map(|d| {
+                    DoPath {
+                        dodir: self.dodir.clone(),
+                        basedir: self.basedir.clone(),
+                        dofile: d
+                    }
+                })
+                .or_else(|| {
+                    // look up one dir for the next defaults
+                    self.up.pop().map(|comp| {
+                        self.dodir.push("..");
+                        self.basedir = Path::new(comp).join(&self.basedir);
+                        self.defaults = DefaultDoFileIter::new(&self.basename);
+                        DoPath {
+                            dodir: self.dodir.clone(),
+                            basedir: self.basedir.clone(),
+                            dofile: self.defaults.next().unwrap()
+                        }
+                    })
+                })
+        }
+    }
+
+
+    struct DefaultDoFileIter {
+        ext: Vec<~[u8]>,
+        base: Vec<~[u8]>
+    }
+    impl DefaultDoFileIter {
+        fn new(filename: &Path) -> DefaultDoFileIter {
+            let fbytes: &[u8] = filename.as_vec();
+            assert!(!fbytes.iter().any(::std::path::is_sep_byte));
+            let parts: Vec<~[u8]> = fbytes.split(|b| *b == DOT).map(|p| p.to_owned()).collect();
+            DefaultDoFileIter {ext: parts, base: Vec::new()}
+        }
+    }
+    impl Iterator<DoFile> for DefaultDoFileIter {
+        fn next(&mut self) -> Option<DoFile> {
+            fn join_dots(v: &Vec<~[u8]>) -> ~[u8] {
+                v.as_slice().connect_vec(&DOT)
+            }
+
+            fn mk_do_name(v: &Vec<~[u8]>) -> ~[u8] {
+                let a4: Vec<~[u8]> =
+                    Some(DEFAULT.to_owned()).move_iter()
+                    .chain(v.clone().move_iter())
+                    .chain(Some(DO.to_owned()).move_iter())
+                    .collect();
+                join_dots(&a4)
+            }
+            self.ext.shift().map(|x| {
+                self.base.push(x);
+                DoFile{
+                    dofile: Path::new(mk_do_name(&self.ext)),
+                    base: Path::new(join_dots(&self.base)),
+                    ext: join_dots(&self.ext)
+                }
+            })
+        }
+    }
+
+
+
     #[cfg(test)]
-    impl ::std::fmt::Show for DefaultDoFile {
+    impl ::std::fmt::Show for DoPath {
         fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-            write!(f.buf, "DDF\\{dofile:\"{}\", base:\"{}\", ext:\"{}\"\\}",
+            write!(f.buf, "DoPath\\{\n  dodir:\"{}\",\n  basedir:\"{}\",\n  dofile:{}\\}",
+                   self.dodir.display(),
+                   self.basedir.display(),
+                   self.dofile)
+        }
+    }
+
+    #[cfg(test)]
+    impl ::std::fmt::Show for DoFile {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            write!(f.buf, "DoFile\\{\n    dofile:\"{}\",\n    base:\"{}\",\n    ext:\"{}\"\\}",
                    self.dofile.display(),
                    self.base.display(),
                    ::std::str::from_utf8(self.ext).unwrap())
         }
     }
 
-    fn default_do_files<'a>(filename: Path) ->
-        ::std::iter::Unfold<'a, DefaultDoFile, DefaultDoFileIterState>
-    {
-        static DOT: u8 = '.' as u8;
-        static DEFAULT: &'static [u8] = bytes!("default");
-        static DO: &'static [u8] = bytes!("do");
+    #[cfg(test)]
+    mod tests {
+        use builder::dofile::{DoPath, DoFile, all_possible_do_files, DefaultDoFileIter};
+        fn p(x: &str) -> Path { return Path::new(x) }
+        fn b(s: &str) -> ~[u8] { s.as_bytes().to_owned() }
 
-        fn join_dots(v: &Vec<~[u8]>) -> ~[u8] {
-            v.as_slice().connect_vec(&DOT)
+        #[cfg(never)]
+        fn dump<T : ::std::fmt::Show>(filename: &str, x: &T) {
+            use std::io::fs;
+            let mut f : fs::File =
+                fs::File::create(&p(filename))
+                .unwrap_or_handle(|e| fail!("create: {}", e));
+            let () = write!(&mut f, "{}", x)
+                .unwrap_or_handle(|e| fail!("write: {}", e));
         }
 
-        fn mk_do_name(v: &Vec<~[u8]>) -> ~[u8] {
-            let a4: Vec<~[u8]> =
-                vec!(DEFAULT.to_owned()).move_iter()
-                .chain(v.clone().move_iter())
-                .chain(vec!(DO.to_owned()).move_iter())
-                .collect();
-            join_dots(&a4)
-        }
-
-        let fbytes: &[u8] = filename.as_vec();
-        assert!(!fbytes.iter().any(::std::path::is_sep_byte));
-        let parts: Vec<~[u8]> = fbytes.split(|b| *b == DOT).map(|p| p.to_owned()).collect();
-
-        ::std::iter::Unfold::new(
-            DefaultDoFileIterState {ext: parts, base: Vec::new()},
-            |st| {
-                match st.ext.shift() {
-                    None => return None,
-                    Some(x) => {
-                        st.base.push(x);
-                        Some(DefaultDoFile{
-                            dofile: Path::new(mk_do_name(&st.ext)),
-                            base: Path::new(join_dots(&st.base)),
-                            ext: join_dots(&st.ext)
-                        })
+        #[test]
+        fn do_file_paths() {
+            let actual =
+                all_possible_do_files(&p("foo/x.y"),
+                                      &p("/bar")).collect();
+            let expected = vec!(
+                DoPath {
+                    dodir: p("foo"),
+                    basedir: p(""),
+                    dofile: DoFile {
+                        dofile: p("x.y.do"),
+                        base: p("x.y"),
+                        ext: b("")
                     }
-                }
-            })
-    }
-
-    struct DefaultDoFileIterState {
-        ext: Vec<~[u8]>,
-        base: Vec<~[u8]>
-    }
-
-    #[test]
-    fn defaults_iter() {
-        let p = Path::new;
-        fn b(s: &str) -> ~[u8] {
-            s.as_bytes().to_owned()
-        }
-        assert_eq!(
-            vec!(
-                DefaultDoFile{
-                    dofile: p("default.foo.bar.c.do"),
-                    base: p("file"),
-                    ext: b("foo.bar.c")
                 },
-                DefaultDoFile{
-                    dofile: p("default.bar.c.do"),
-                    base: p("file.foo"),
-                    ext: b("bar.c")
-                },
-                DefaultDoFile{
-                    dofile: p("default.c.do"),
-                    base: p("file.foo.bar"),
-                    ext: b("c")
-                },
-                DefaultDoFile{
-                    dofile: p("default.do"),
-                    base: p("file.foo.bar.c"),
-                    ext: ~[]
-                }
-                ),
-            default_do_files(p("file.foo.bar.c")).collect());
-    }
-}
-
-mod old_dofile {
-    /* Finding do-files:
-     *
-     * for a file foo/bar/baz.a.b.c the following .do-files should be
-     * looked for in this order:
-     *  - foo/bar/baz.a.b.c.do
-     *  - foo/bar/default.a.b.c.do
-     *  - foo/bar/default.b.c.do
-     *  - foo/bar/default.c.do
-     *  - foo/bar/default.do
-     *  - foo/default.a.b.c.do
-     *  - foo/default.b.c.do
-     *  - foo/default.c.do
-     *  - foo/default.do
-     *  - default.a.b.c.do
-     *  - default.b.c.do
-     *  - default.c.do
-     *  - default.do
-     *  - ../default.a.b.c.do
-     *  - ../default.b.c.do
-     *  - ../default.c.do
-     *  - ../default.do
-     *  - and so on...
-     */
-    #[test]
-    fn do_file_search() {
-        let mut dofile_iter = DoFileIter::new("foo/bar/baz.a.b.c");
-        assert_eq!(Some(~"foo/bar/baz.a.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/bar/default.a.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/bar/default.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/bar/default.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/bar/default.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/default.a.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/default.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/default.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"foo/default.do"), dofile_iter.next());
-        assert_eq!(Some(~"default.a.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"default.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"default.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"default.do"), dofile_iter.next());
-        assert_eq!(Some(~"../default.a.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"../default.b.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"../default.c.do"), dofile_iter.next());
-        assert_eq!(Some(~"../default.do"), dofile_iter.next());
-    }
-
-    struct DoFileIter {
-        file: Option<~str>,
-        extensions: Vec<~str>,
-        dir: ~str,
-        dir_iter: DirIter,
-        do_iter: DefaultDoIter
-    }
-
-    impl Iterator<~str> for DoFileIter {
-        fn next(&mut self) -> Option<~str> {
-            // If we haven't yielded the file, do so.
-            let file = self.file.clone();
-            self.file = None;
-            match file {
-                None => (),
-                Some(f) => {
-                    return Some(f + ".do");
-                }
-            }
-            // Try the next default
-            match self.do_iter.next() {
-                Some(f) => {
-                    if self.dir.len() > 0 {
-                        Some(self.dir.clone() + "/" + f)
-                    } else {
-                        Some(f)
+                DoPath {
+                    dodir: p("foo"),
+                    basedir: p(""),
+                    dofile: DoFile {
+                        dofile: p("default.y.do"),
+                        base: p("x"),
+                        ext: b("y")
                     }
-                }
-                // We are out of defaults at this level, go down one
-                // and freshen the default-do iter.
-                None => match self.dir_iter.next() {
-                    None => None, // No more dirs, we are done
-                    Some(d) => {
-                        self.dir = d;
-                        self.do_iter = DefaultDoIter::new(self.extensions.clone());
-                        self.next() // recurse to get the value
+                },
+                DoPath {
+                    dodir: p("foo"),
+                    basedir: p(""),
+                    dofile: DoFile {
+                        dofile: p("default.do"),
+                        base: p("x.y"),
+                        ext: b("")
                     }
-                }
-            }
+                },
+                DoPath {
+                    dodir: p("foo/.."),
+                    basedir: p("foo"),
+                    dofile: DoFile {
+                        dofile: p("default.y.do"),
+                        base: p("x"),
+                        ext: b("y")
+                    }
+                },
+                DoPath {
+                    dodir: p("foo/.."),
+                    basedir: p("foo"),
+                    dofile: DoFile {
+                        dofile: p("default.do"),
+                        base: p("x.y"),
+                        ext: b("")
+                    }
+                },
+                DoPath {
+                    dodir: p("foo/../.."),
+                    basedir: p("bar/foo"),
+                    dofile: DoFile {
+                        dofile: p("default.y.do"),
+                        base: p("x"),
+                        ext: b("y")
+                    }
+                },
+                DoPath {
+                    dodir: p("foo/../.."),
+                    basedir: p("bar/foo"),
+                    dofile: DoFile {
+                        dofile: p("default.do"),
+                        base: p("x.y"),
+                        ext: b("")
+                    }
+                });
+            assert_eq!(expected, actual);
         }
-    }
 
-    impl DoFileIter {
-        fn new(file: &str) -> DoFileIter {
-            let exts : Vec<~str> = file.split('.').skip(1).map(|x| x.to_owned()).collect();
-            let mut diter = DirIter::new(file);
-
-            DoFileIter {
-                file: Some(file.to_owned()),
-                do_iter: DefaultDoIter::new(exts.clone()),
-                extensions: exts,
-                dir: diter.next().unwrap(),
-                dir_iter: diter,
-            }
+        #[test]
+        fn defaults_iter() {
+            assert_eq!(
+                vec!(
+                    DoFile{
+                        dofile: p("default.foo.bar.c.do"),
+                        base: p("file"),
+                        ext: b("foo.bar.c")
+                    },
+                    DoFile{
+                        dofile: p("default.bar.c.do"),
+                        base: p("file.foo"),
+                        ext: b("bar.c")
+                    },
+                    DoFile{
+                        dofile: p("default.c.do"),
+                        base: p("file.foo.bar"),
+                        ext: b("c")
+                    },
+                    DoFile{
+                        dofile: p("default.do"),
+                        base: p("file.foo.bar.c"),
+                        ext: ~[]
+                    }
+                    ),
+                DefaultDoFileIter::new(&p("file.foo.bar.c")).collect());
         }
-    }
-
-    struct DirIter {
-        dir_elts: Vec<~str>,
-        down_depth: uint
-    }
-
-    impl Iterator<~str> for DirIter {
-        fn next(&mut self) -> Option<~str> {
-            let d = self.dir_elts.connect("/");
-            if self.dir_elts.pop().is_none() {
-                // d is empty, and we are iterating on [../]*down_depth
-                let downs = Vec::from_fn(self.down_depth, |_| ~"..");
-                self.down_depth += 1;
-                Some(downs.connect("/"))
-            } else {
-                Some(d)
-            }
-        }
-    }
-
-    impl DirIter {
-        fn new(file: &str) -> DirIter {
-            let mut delts : Vec<~str> = file.split('/').map(|x| x.to_owned()).collect();
-            delts.pop(); // The file part
-            DirIter {
-                down_depth: 0,
-                dir_elts: delts
-            }
-        }
-    }
-
-    #[test]
-    fn dir_iter() {
-        assert_eq!(vec!(~"foo/bar",
-                        ~"foo",
-                        ~"",
-                        ~"..",
-                        ~"../..",
-                        ),
-                   DirIter::new("foo/bar/baz.c").take(5).collect());
-    }
-
-
-    struct DefaultDoIter {
-        exts: Vec<~str>
-    }
-
-    impl Iterator<~str> for DefaultDoIter {
-        fn next(&mut self) -> Option<~str> {
-            let e = self.exts.connect(".");
-            if self.exts.shift().is_none() {
-                None
-            } else {
-                Some("default." + e)
-            }
-        }
-    }
-
-    impl DefaultDoIter {
-        fn new(exts: Vec<~str>) -> DefaultDoIter {
-            let mut e = exts;
-            e.push(~"do");
-            DefaultDoIter { exts: e }
-        }
-    }
-
-    #[test]
-    fn default_do_iter() {
-        assert_eq!(vec!(~"default.foo.bar.c.do",
-                        ~"default.bar.c.do",
-                        ~"default.c.do",
-                        ~"default.do"),
-                   DefaultDoIter::new(vec!(~"foo", ~"bar", ~"c")).collect());
     }
 }
