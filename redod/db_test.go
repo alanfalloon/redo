7eb2d83dc6fec2c7f1d0b75f76585930ef172369
Alan Falloon <alan.falloon@gmail.com>
2014-08-28 23:27:35 -0400

    tdbconn() get deps

--- redod/db_test.go
+++ redod/db_test.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"testing"
+	"database/sql"
 )
 
-func TestX(t *testing.T) {
-	t.Error("implement me")
+func tdbconn() *db {
+	_conn, err := sql.Open("sqlite3", ":memory:")
+	check(err)
+	conn := db{_conn, 0}
+	conn.xExec("PRAGMA foreign_keys = ON;")
+	conn.xExec("PRAGMA temp_store = MEMORY;")
+	dbinit(conn)
+	return &conn
+}
+
+func TestDbinit(t *testing.T) {
+	_ = tdbconn()
+}
+
+func TestDemand(t *testing.T) {
 }
